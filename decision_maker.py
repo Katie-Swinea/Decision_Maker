23b4fb0575000168991cd8d2724eab83abc8c402
Katie-Swinea <111701542+Katie-Swinea@users.noreply.github.com>
2025-03-22 16:37:42 -0500

    Error Checks and Input Formatting

--- decision_maker.py
+++ decision_maker.py
@@ -1,6 +1,7 @@
 import json
 import os
 import random
+import textwrap
 
 DECISION_FILE = 'decisions.json'
 
@@ -13,11 +14,11 @@ def load_data():
 
 	return {}
 
-def check_preferences():
-	preferences = ""
+def check_preferences(preferences):
 	while preferences != 'num' and preferences != 'history' and preferences != 'mood' and preferences != 'default' :
-		settings = input("Please choose if you want your choice to be made at random, based on a number ranking system, based on the context surrounding the choice or based on the history of your old choices. ")
-		if settings.lower() == 'num' or settings.lower() == 'number' or settings.lower() == 'n' or settings.lower() == '#' or preferences.lower() == 'rank' or preferences.lower() == 'ranking' or preferences.lower() == 'system' :
+		msg = textwrap.fill("Please choose if you want your choice to be made at random, based on a number ranking system, based on the context surrounding the choice, or based on the history of your old choices. ", width=100)
+		settings = input("\n"+msg+" ")
+		if settings.lower() == 'num' or settings.lower() == 'number' or settings.lower() == 'n' or settings.lower() == '#' or settings.lower() == 'rank' or settings.lower() == 'ranking' or settings.lower() == 'system' :
 			preferences = 'num'
 		if settings.lower() == 'mood' or settings.lower() == 'scene' or settings.lower() == 'scenario' or settings.lower() == 'circumstance' or settings.lower() == 's' or settings.lower() == 'context' :
 			preferences = 'mood'
@@ -33,82 +34,108 @@ def add_problem(input):
 		with open(DECISION_FILE, 'w') as f:
 			json.dump(decisions, f)
 
-def add_solution(inPut):
+def add_solution(inPut, preferences):
 	if inPut in decisions :
-		solution = input("Enter an option for the problem. Enter 'stop' when you are finished. ")
+		msg = textwrap.fill("Enter an option for the problem. Enter 'stop' when you are finished.", width=100)
+		solution = input("\n"+msg+" ")
 		if preferences == 'default' :
 			while solution.lower() != 'stop' :
 				if solution not in decisions[inPut]:
-					decisions[inPut].append({"solutions": solution, "ranking": [], "mood": [], "history": '0'})
+					decisions[inPut].append({"solutions": solution, "ranking": [], "mood": [], "history": 0})
 				with open(DECISION_FILE, 'w') as f:
 					json.dump(decisions, f)
-				solution = input("Enter an option for the problem. Enter 'stop' when you are finished. ")
+				msg = textwrap.fill("Enter an option for the problem. Enter 'stop' when you are finished.", width=100)
+				solution = input("\n"+msg+" ")
 		if preferences == 'num' :
 			while solution.lower() != 'stop' :
-				rank = input("Enter a ranking for that option. Remember more than one option can have the same ranking. ")
-				if solutions not in decisions[inPut]:
-					decisions[inPut].append({"solutions": solution, "ranking": rank, "mood": [], "history": '0'})
+				msg = textwrap.fill("Enter a ranking for that option. Remember more than one option can have the same ranking.", width=100)
+				rank = input("\n"+msg+" ")
+				if solution not in decisions[inPut]:
+					decisions[inPut].append({"solutions": solution, "ranking": rank, "mood": [], "history": 0})
 				with open(DECISION_FILE, 'w') as f:
 					json.dump(decisions, f)
-				solution = input("Enter an option for the problem. Enter 'stop' when you are finished. ")
+				msg = textwrap.fill("Enter an option for the problem. Enter 'stop' when you are finished.", width=100)
+				solution = input("\n"+msg+" ")
 		if preferences == 'mood' :
 			while solution.lower() != 'stop' :
-				mood = input("Enter a mood corresponding to this option. More than one option can have the same mood. ")
+				msg = textwrap.fill("Enter a mood corresponding to this option. More than one option can have the same mood.", width=100)
+				mood = input("\n"+msg+" ")
 				if solution not in decisions[inPut]:
-					decisions[inPut].append({"solutions": solution, "ranking": [], "mood": mood, "history": '0'})
+					decisions[inPut].append({"solutions": solution, "ranking": [], "mood": mood, "history": 0})
 				with open(DECISION_FILE, 'w') as f:
 					json.dump(decisions, f)
-				solution = input("Enter an option for the problem. Enter 'stop' when you are finished. ")
-def get_solution(problem, decisions):
+				msg = textwrap.fill("Enter an option for the problem. Enter 'stop' when you are finished.", width=100)
+				solution = input("\n"+msg+" ")
+
+def get_solutions(problem, decisions, preferences, mood):
+	if problem not in decisions :
+		add_problem(problem)
 	while problem.lower() != 'exit' :
-		if problem not in decisions:
-                        add_problem(problem)
-                if decisions[problem] == []:
-                        add_solution(problem)
-                else :
-                        while (preferences == 'num' and decisions[problem][1]["ranking"] == []) or (preferences == 'mood' and decisions[problem][0]["mood"] == []):
-                                temp_problem = problem
-                                problem = input("Enter a problem that matches the preferences you have set for this session. To change your prefernces, enter 'cp' so the preferences match the problem. To add t>
-                                if problem.upper() == 'CP' or problem.lower() == 'change' or problem.lower() == 'preferences' or problem.lower() == 'change preferences' :
-                                        problem = temp_problem
-                                        preferences = input("To do a number ranking, enter 'num', to do a circumstance ranking, enter 'circum', to do a choice based on your history, enter 'history', and to do >
-                                        if preferences.lower() == 'circum' or preferences.lower() == 'circ' or preferences.lower() == 'circumstance' or preferences.lower() == 'circumstances':
-                                                preferences = 'mood'
-                                        if preferences.lower() == 'random' or preferences.lower() == 'rand' or preferences.lower() == 'r' :
-                                                preferences = 'default'
-                                        while preferences != 'num' and preferences != 'mood' and preferences != 'history' and preferences != 'default' :
-                                                preferences = input("To do a number ranking, enter 'num', to do circumstance based ranking, enter 'circum', to do a choice based on your history, enter 'history'>
-                                if problem.upper() == 'AP' or problem.lower() == 'add problem' or problem.lower() == 'add to problem' or problem.lower() == 'add' or problem.lower() == 'problem' :
-                                        if preferences == 'num' :
-                                                problem = temp_problem
-                                                for solution in decisions[problem] :
-                                                        print(solution["solutions"])
-                                                        solution["ranking"] = input("Rank this solution. ")
-                                                        with open (DECISION_FILE, 'w') as f:
-                                                                json.dump(decisions, f)
-                                        if preferences == 'mood' :
-                                                problem = temp_problem
-                                                for solution in decisions[problem] :
-                                                        print(solution["solutions"])
-                                                        solution["mood"] = input("Enter the mood that is preferred for this solution. ")
-                                                        with open (DECISION_FILE, 'w') as f:
-                                                                json.dump(decisions, f)
-
-                        solution = input("Do you want to add options for the decision? ")
-                        if solution.upper() == 'Y' or solution.upper() == 'YES' or solution.upper() == 'YEAH' or solution.upper() == 'YE' :
-                                add_solution(problem)
-
-                result = get_choice(problem, mood)
-                if result == 'nothing' :
-                        print("No results were found. Please review your input. Make sure the mood exists if you set your preferences to context. Make sure you did not accidentally create a blank solution.")
-                else:
-                        print(result["solutions"])
-                        loop = input("Do you want to regenerate the solution?")
-                        if loop.lower() != 'y' and loop.lower() != 'yes' and loop.lower() != 'ye' and loop.lower() != 'yeah' :
-                                problem = "exit"
+		while decisions[problem] == [] and preferences != 'mood' and preferences != 'default' and preferences != 'num':
+			msg = textwrap.fill("You cannot use history with a new or empty problem because it has no previous data to use.", width=100)
+			print("\n"+msg)
+			msg = textwrap.fill("Please choose a different preference.", width=100)
+			preferences = input("\n"+msg+" ")
+			if preferences.lower() == 'num' or preferences.lower() == 'number' or preferences.lower() == 'n' or preferences.lower() == '#' or preferences.lower() == 'rank' or preferences.lower() == 'ranking' :
+				preferences = 'num'
+			if preferences.lower() == 'mood' or preferences.lower() == 'scene' or preferences.lower() == 'scenario' or preferences.lower() == 'circumstance' or preferences.lower() == 's' or preferences.lower() == 'context' :
+				preferences = 'mood'
+				msg = textwrap.fill("Please enter the mood that will be affecting your decision today.", width=100)
+				mood = input("\n"+msg+" ")
+			if preferences.lower() == 'default' or preferences.lower() == 'r' or preferences.lower() == 'rand' or preferences.lower() == 'random' :
+				preferences = 'default'
+		if decisions[problem] == [] :
+			add_solution(problem, preferences)
+		else:
+			while (preferences == 'num' and decisions[problem][0]["ranking"] == []) or (preferences == 'mood' and decisions[problem][0]["mood"] == []):
+				temp_problem = problem
+				msg = textwrap.fill("Enter a problem that matches the preferences you have set for this sission. To change your preferences, enter 'cp' so the preferences match the problem. To add options to your problem, enter 'ap'.", width=100)
+				problem = input("\n"+msg+" ")
+				if problem.upper() == 'CP' or problem.lower() == 'change' or problem.lower() == 'preferences' or problem.lower() == 'change preferences' :
+					problem = temp_problem
+					msg = textwrap.fill("To do a number ranking, enter 'num', to do a cicrumstance ranking, enter 'circum', to do a choice based on your history, enter 'history', to have a random choice made, enter 'random'.", width=100)
+					preferences = input("\n"+msg+" ")
+					if preferences.lower() == 'circum' or preferences.lower() == 'circ' or preferences.lower() == 'circumstance' or preferences.lower() == 'circumstances' :
+						preferences = 'mood'
+						msg = textwrap.fill("Please enter the mood that will be affecting your decision today.", width=100)
+						mood = input("\n"+msg+" ")
+					if preferences.lower() == 'random' or preferences.lower() == 'rand' or preferences.lower() == 'r' :
+						preferences = 'default'
+					while preferences != 'num' and preferences != 'mood' and preferences != 'history' and preferences != 'default' :
+						msg = textwrap.fill("To do a number ranking, enter 'num', to do a circumstance based ranking, enter 'circum', to do a choice based on your history, enter 'history', to make a random choice, enter 'random'.", width=100)
+						print("\n"+msg)
+				if problem.upper() == 'AP' or problem.lower() == 'add problem' or problem.lower() == 'add to problem' or problem.lower() == 'add' or problem.lower() == 'problme' :
+					if preferences == 'num' :
+						problem = temp_problem
+						for solution in decisions[problem] :
+							print(solution["solutions"])
+							solution["ranking"] = input("\nRank this solution. ")
+							with open (DECISION_FILE, 'w') as f:
+								json.dump(decisions, f)
+					if preferences == 'mood' :
+						problem = temp_problem
+						for solution in decisions[problem] :
+							print("\n"+solution["solutions"])
+							solution["mood"] = input("\nEnter the mood that is preferred for this solution. ")
+							with open (DECISION_FILE, 'w') as f:
+								json.dump(decisions, f)
+			solution = input("\nDo you want to add an option for the decision? ")
+			if solution.upper() == 'Y' or solution.upper() == 'YES' or solution.upper() == 'YEAH' or solution.upper() == 'YE' :
+				add_solution(problem, preferences)
+		result = get_choice(problem, mood, preferences)
+		if result == 'nothing' :
+			msg = textwrap.fill("No results were found. Please review your input. Make sure the mood exits if you set your preferences to context. Make sure you did not accidentally create a problem with no solutions.", width=100)
+			print("\n"+msg)
+		elif result == 'inconlusive' :
+			print("\nNo single solution has been favored over the others.")
+		else:
+			print(result["solutions"])
+			loop = input("\nDo you want to regenerate the solution? ")
+			if loop.lower() != 'y' and loop.lower() != 'yes' and loop.lower() != 'ye' and loop.lower() != 'yeah' :
+				problem = "exit"
 	return result["solutions"]
 
-def get_choice(input, mood):
+def get_choice(input, mood, preferences):
 	if preferences == 'default':
 		if decisions[input] == []:
 			return 'nothing'
@@ -121,30 +148,63 @@ def get_choice(input, mood):
 		else:
 			return random.choice(solutions)
 	if preferences == 'mood':
-		solutions = [solution for solution in decisions[input] if solution["mood"] == mood]
+		solutions = [solution for solution in decisions[input] if solution["mood"].lower() == mood.lower()]
 		if solutions == []:
 			return 'nothing'
 		else :
 			return random.choice(solutions)
+	if preferences == 'history':
+		total = 0
+		length = 0
+		result = []
+		for solution in decisions[input]:
+			total = total + solution["history"]
+			length += 1
+		if total == 0 or length == 0 :
+			return 'nothing'
+		for solution in decisions[input]:
+			percent = solution["history"]/total
+			if percent >= 1/length:
+				result.append(solution)
+		if result == []:
+			return 'inconclusive'
+		else :
+			return random.choice(result)
 
 def main():
 	global decisions
-	global preferences
-	preferences = check_preferences()
-	if preferences == 'mood' :
-		mood = input("Please enter the mood that will be affecting your decision today. ")
-	else :
-		mood = ""
+	preferences = ""
+	preferences = check_preferences(preferences)
 	decisions = load_data()
-	problem = input("Enter the problem you want a decision to be made for. Enter 'exit' to stop running the program. ")
-	while problem.lower() != 'exit' :
-		solution = get_solution(problem, decisions)
-		temp = int(decisions[problem][solution]["history"])
-		temp += 1
-		decisions[problem][solution]["history"] = str(temp)
-		with open (DECISION_FILE, 'w') as f:
-			json.dump(decisions, f)
-		problem = input("Enter the problem you want a decision to be made for. Enter 'exit' to stop. ")
+	msg = textwrap.fill("Enter the problem you want a decision to be made for. Enter 'exit' to stop running the program.", width=100)
+	problem = input("\n"+msg+" ")
+	while problem.lower() != 'exit':
+		if preferences == 'mood' :
+			msg = textwrap.fill("Please enter the mood that will be affecting your decision today.", width=100)
+			mood = input("\n"+msg+" ")
+		else :
+			mood = ""
+		solution = get_solutions(problem, decisions, preferences, mood.lower())
+		for index in decisions[problem] :
+			if index["solutions"] == solution :
+				index["history"] += 1
+				with open(DECISION_FILE, 'w') as f:
+					json.dump(decisions, f)
+		msg = textwrap.fill("Do you want to change your preferences before you enter a new problem.", width=100)
+		temp_preferences = input("\n"+msg+" ")
+		if temp_preferences.lower() == 'y' or temp_preferences.lower() == 'yes' or temp_preferences.lower == 'ye' or temp_preferences.lower() == 'yeah' :
+			preferences = input("\nEnter your new preferences. ")
+			while preferences != 'num' and preferences != 'mood' and preferences != 'history' and preferences != 'default' :
+				if preferences.lower() == 'num' or preferences.lower() == 'number' or preferences.lower() == '#' or preferences.lower() == 'rank' or preferences.lower() == 'ranking':
+					preferences = 'num'
+				if preferences.lower() == 'mood' or preferences.lower() == 'scenario' or preferences.lower() == 'context' or preferences.lower == 'circumstance' :
+					preferences = 'mood'
+				if preferences.lower() == 'history' or preferences.lower() == 'previous' or preferences.lower() == 'old' :
+					preferences = 'history'
+				if preferences.lower() == 'default' or preferences.lower() == 'random' or preferences.lower() == 'rand' :
+					preferences = 'default'
+		msg = textwrap.fill("Enter the problem you want a decision to be made for. Enter 'exit' to stop running the program.", width=100)
+		problem = input("\n"+msg+" ")
 
 if __name__ == "__main__":
 	main()
